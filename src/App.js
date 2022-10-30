d9e11e7a373c26bc80e79427be71f8df3dffcc94
Vladimir RM <shvdrummer@gmail.com>
2022-10-30 12:30:34 +0200

    add in the app   <Header />       <AddTodo/>       <TodoList/>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo/AddTodo";
 import Header from "./components/Header/Header";
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddTodo/>
+      <TodoList/>
     </div>
   );
 }
