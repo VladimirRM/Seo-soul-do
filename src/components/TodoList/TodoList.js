1abbfadf75ff14344c6e9704d2e323aaceabdbc2
Vladimir RM <shvdrummer@gmail.com>
2022-10-30 13:48:14 +0200

    check function delete

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const TodoList = ({todo,setTodo}) => {
-
-function deleteTodo  (){
-console.log("delete");
-}
+const TodoList = ({ todo, setTodo }) => {
+  function deleteTodo(id) {
+    let newTodo = [...todo].filter(item=> item.id!=id)
+    setTodo(newTodo)
+  }
 
   return (
     <div>
-        {
-            todo.map(item=>(
-                <div key={item.id}>
-                <div>{item.title}</div>
-                <button onClick={()=>deleteTodo(note.id)}>Delete</button>
-                </div>
-            ))
-        }
+      {todo.map((item) => (
+        <div key={item.id}>
+          <div>{item.title}</div>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
