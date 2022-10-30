eabeb6466aa6d854dbbddb6574eb75d13782f932
Vladimir RM <shvdrummer@gmail.com>
2022-10-30 12:38:07 +0200

    add main array id: 1,       title: "first todo",       status:true

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import AddTodo from "./components/AddTodo/AddTodo";
@@ -6,14 +6,19 @@ import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
 
 function App() {
-
-const [todo,setTodo] = useState("")
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "first todo",
+      status:true
+    },
+  ]);
 
   return (
     <div className="App">
       <Header />
-      <AddTodo/>
-      <TodoList/>
+      <AddTodo />
+      <TodoList />
     </div>
   );
 }
