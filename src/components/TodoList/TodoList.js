ed9f8e8e7ad8c79ac3672aa15e6b173f60cfec2a
Vladimir RM <shvdrummer@gmail.com>
2022-10-30 13:54:41 +0200

    create second button

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -12,6 +12,7 @@ const TodoList = ({ todo, setTodo }) => {
         <div key={item.id}>
           <div>{item.title}</div>
           <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
         </div>
       ))}
     </div>
