40d0a01f344dac801b6823c4617fa7bcc9e8363d
Vladimir RM <shvdrummer@gmail.com>
2022-10-31 13:28:55 +0200

    create func editTodo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -14,6 +14,9 @@ const TodoList = ({ todo, setTodo }) => {
     })
     setTodo(newTodo)
   }
+    function editTodo(){
+        
+    }
 
   return (
     <div>
@@ -21,6 +24,7 @@ const TodoList = ({ todo, setTodo }) => {
         <div key={item.id}>
           <div>{item.title}</div>
           <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <button onClick={() => editTodo(item.id)}>Edit</button>
           <button onClick={() => statusTodo(item.id)}>Close/Open</button>
         </div>
       ))}
