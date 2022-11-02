c49a6a041154b1f27530043cabccb03aa8437401
Vladimir RM <shvdrummer@gmail.com>
2022-11-02 14:12:39 +0200

    add FormControl

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,26 +1,34 @@
-import React,{useState} from "react";
-import { v4 as uuidv4 } from 'uuid'
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Row, Col,Button , FormControl} from "react-bootstrap";
 
-const AddTodo = ({todo,setTodo}) => {
-       const [value,setValue]=useState('')
+const AddTodo = ({ todo, setTodo }) => {
+  const [value, setValue] = useState("");
 
+  function saveTodo() {
+    setTodo([
+      ...todo,
+      {
+        id: uuidv4(),
+        title: value,
+        status: true,
+      },
+    ]);
+    setValue("");
+  }
 
-      function saveTodo(){
-        setTodo([
-            ...todo,{
-                id:uuidv4(),
-                title: value,
-                status:true
-            }
-        ])
-        setValue('')
-      }
-
-  return <div>
-    <input placeholder="Enter todo" value={value}
-    onChange={(e)=> setValue(e.target.value)}/>
-    <button onClick={saveTodo}>Save</button>
-  </div>;
+  return (
+    <Row>
+      <Col>
+        < FormControl
+          placeholder="Enter todo"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button variant="outline-secondary"onClick={saveTodo}>Save</Button>
+      </Col>
+    </Row>
+  );
 };
 
 export default AddTodo;
