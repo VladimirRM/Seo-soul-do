fd3d8214a685e645f7c9de36f721c8cfd45186b1
Vladimir RM <shvdrummer@gmail.com>
2022-12-11 10:20:30 +0200

    add func todoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
+import AddTodo from "./components/AddTodo/AddTodo";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <AddTodo/>
     </div>
   );
 };
