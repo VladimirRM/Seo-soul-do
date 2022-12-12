5725e403e360b5832f39f51d678ab437a70d6909
Vladimir RM <shvdrummer@gmail.com>
2022-12-12 14:51:22 +0200

    create 3 main obj todo

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import Header from './components/Header/Header'
 import TodoList from './components/TodoList/TodoList'
 import AddTodo from './components/AddTodo/AddTodo'
 import './App.css'
-import Header from './components/Header/Header'
+import { First } from 'react-bootstrap/esm/PageItem'
+
 
 const App = () => {
-  const [todo,setTodo] = useState([])
+  const [todo,setTodo] = useState([
+    {
+      id:1,
+      title: 'first todo',
+      status: true,
+
+    },
+    {
+      id:2,
+      title: 'second todo',
+      status: true,
+
+    },
+    {
+      id:3,
+      title: 'third todo',
+      status: false,
+
+    },
+  ])
 
   return (
     <div>
