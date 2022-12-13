9ddffd4079f68567e4969bef052be2fcb35f4374
Vladimir RM <shvdrummer@gmail.com>
2022-12-13 09:18:45 +0200

    add func savetodo

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,13 +1,22 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const AddTodo = () => {
+  const [value, setValue] = useState("");
+
+function saveTodo
+
+
   return (
     <div>
-      <input type="text"
-      placeholder='Enter something...' 
-      value={value}/>
+      <input
+        type="text"
+        placeholder="Enter something..."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={saveTodo}>Save</button>
     </div>
-  )
-}
+  );
+};
 
-export default AddTodo
+export default AddTodo;
