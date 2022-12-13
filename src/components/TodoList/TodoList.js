d5cc1765a623dc56f4c58b91099d65ed9cbcc5be
Vladimir RM <shvdrummer@gmail.com>
2022-12-13 09:46:06 +0200

    add func deletetodo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,27 +2,15 @@ import React from "react";
 
 const TodoList = ({ todo, setTodo }) => {
   function deleteTodo(id) {
-    const newTodo = [...todo].filter((item) => item.id !== id);
-    setTodo(newTodo);
+    setTodo([...todo].filter((item) => item.id != id));
   }
-  function statusTodo(id) {
-    const newTodo = [...todo].filter((item) => {
 
-      if (item.id == id) {
-        item.status = !item.status;
-      }
-      return item;
-    });
-    setTodo(newTodo);
-  }
-console.log(todo);
   return (
     <div>
       {todo.map((item) => (
         <div key={item.id}>
           <div>{item.title}</div>
-          <button onClick={() => deleteTodo(item.id)}>delete</button>
-          <button onClick={() => statusTodo(item.id)}>Open/Close</button>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
         </div>
       ))}
     </div>
