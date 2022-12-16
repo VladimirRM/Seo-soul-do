f956b8285d4cbe146684eb1079505ceecbc056d9
Vladimir RM <shvdrummer@gmail.com>
2022-12-16 20:19:59 +0200

    add input

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,31 +1,9 @@
-import React, { useState } from "react";
+import React from 'react'
 
-const AddTodo = ({ todo, setTodo }) => {
-  const [value, setValue] = useState("");
-
-  function saveTodo() {
-    setTodo([
-      ...todo,
-      {
-        id: Math.random(),
-        title: value,
-        status: true,
-      },
-    ]);
-    setValue("");
-  }
-
+const AddTodo = () => {
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter something..."
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-      />
-      <button onClick={saveTodo}>Save</button>
-    </div>
-  );
-};
+    <div>AddTodo</div>
+  )
+}
 
-export default AddTodo;
+export default AddTodo
