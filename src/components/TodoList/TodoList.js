b760d82a379d5e611c77f0a8be51c442196d48c4
Vladimir RM <shvdrummer@gmail.com>
2022-12-16 20:48:52 +0200

    add button statusTodo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 const TodoList = ({ todo, setTodo }) => {
+  function deleteTodo(id) {
+    const newTodo = [...todo].filter((item) => item.id != id);
+    setTodo(newTodo);
+  }
 
-
-
-  
   return (
     <div>
       {
         <div key={item.id}>
           <div>{item.title}</div>
           <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <button onClick={() => statusTodo(item.id)}>Close/Open</button>
         </div>
       }
     </div>
