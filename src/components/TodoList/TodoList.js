9a529266e45005ac6a8753a5e43e2691292e29f2
Vladimir RM <shvdrummer@gmail.com>
2022-12-16 20:45:44 +0200

    add todo form

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,34 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
 const TodoList = ({ todo, setTodo }) => {
-  const [edit, setEdit] = useState();
-  function deleteTodo(id) {
-    let newTodo = [...todo].filter((item) => item.id != id);
-    setTodo(newTodo);
-  }
 
-  function statusTodo(id) {
-    let newTodo = [...todo].filter((item) => {
-      if (item.id === id) {
-        item.status = !item.status;
-      }
-      return item;
-    });
-    setTodo(newTodo);
-  }
 
-  function editTodo(id) {}
 
+  
   return (
     <div>
-      {todo.map((item) => (
+      {
         <div key={item.id}>
           <div>{item.title}</div>
-          <button onClick={() => statusTodo(item.id)}>Delete</button>
-          <button onClick={() => editTodo(item.id)}>Edit</button>
-          <button onClick={() => deleteTodo(item.id)}>Open/Close</button>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
         </div>
-      ))}
+      }
     </div>
   );
 };
