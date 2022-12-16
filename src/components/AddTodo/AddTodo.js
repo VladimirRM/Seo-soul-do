e3a24dfaf0cf3bf266cbff4ec4bdaf0936e24176
Vladimir RM <shvdrummer@gmail.com>
2022-12-16 20:38:13 +0200

    add input and connect value

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const AddTodo = () => {
+const [value,setValue] = useState('')
+
   return (
-    <div>AddTodo</div>
+    <div>
+      <input type="text"
+      value={value}
+      placeholder='Enter something...'
+      onChange={(e)=>setValue(e.target.value)} />
+      <button onClick={saveTodo}></button>
+    </div>
   )
 }
 
