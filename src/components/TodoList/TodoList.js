d372519b73d897a7288eb6839be8451f6522317a
Vladimir RM <shvdrummer@gmail.com>
2022-12-17 09:49:36 +0200

    create input for block edit

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoList = ({ todo, setTodo }) => {
+  const [value, setValue] = useState("");
+  const [edit, setEdit] = useState(null);
+
   function deleteTodo(id) {
     const newTodo = [...todo].filter((item) => item.id != id);
     setTodo(newTodo);
@@ -14,13 +17,36 @@ const TodoList = ({ todo, setTodo }) => {
     });
   }
 
+  function editTodo(id, title) {
+    setEdit(id);
+    setValue(title);
+  }
+
   return (
     <div>
       {todo.map((item) => (
         <div key={item.id}>
-          <div>{item.title}</div>
-          <button onClick={() => deleteTodo(item.id)}>Delete</button>
-          <button onClick={() => statusTodo(item.id)}>Close/Open</button>
+          {edit == item.id ? (
+            <div>
+              <input value={value} />
+            </div>
+          ) : (
+            <div>{item.title}</div>
+          )}
+
+          {edit == item.id ? (
+            <div>
+              <button>Save</button>
+            </div>
+          ) : (
+            <div>
+              <button onClick={() => deleteTodo(item.id)}>Delete</button>
+              <button onClick={() => editTodo(item.id, item.title)}>
+                Edit
+              </button>
+              <button onClick={() => statusTodo(item.id)}>Close/Open</button>
+            </div>
+          )}
         </div>
       ))}
     </div>
