eb8fa1f0d56c56062b13f25809d33df1e6a7ab96
Vladimir RM <shvdrummer@gmail.com>
2022-12-18 16:00:12 +0200

    add func delete Todo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,25 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const TodoList = () => {
-  const [edit,setEdit]=useState(null)
-  
+const TodoList = ({ todo, setTodo }) => {
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
+
+  function deleteTodo(id) {
+    const newTodo = [...todo].filter((item) => item.id !== id);
+    setTodo(newTodo);
+  }
+
   return (
-    <div>TodoList</div>
-  )
-}
+    <div>
+      {todo.map((item) => (
+        <div key={item.id}>
+          <div>{item.title}</div>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <button onClick={() => statusTodo(item.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
