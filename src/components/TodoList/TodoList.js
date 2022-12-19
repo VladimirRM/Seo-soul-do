b24818b53ca1dd40a62df50f1da46d6929a9be36
Vladimir RM <shvdrummer@gmail.com>
2022-12-19 09:40:40 +0200

    add func statusTodo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,24 +1,29 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const TodoList = ({todo,setTodo}) => {
+const TodoList = ({ todo, setTodo }) => {
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
 
-const [edit,setEdit]=useState(null)
-const [value,setValue]= useState('')
+  function deleteTodo(id) {
+    const newTodo = [...todo].filter((item) => item.id !== id);
+    setTodo(newTodo);
+  }
 
-function deleteTodo(id){
-       const newTodo = []
-}
+  function statusTodo(){
+    
+  }
 
   return (
     <div>
-         {todo.map((item)=>(
-          <div key={item.id}>
-            <div>{item.title}</div>
-            <button onClick={()=>deleteTodo(item.id)}>Delete</button>
-          </div>
-         ))}
+      {todo.map((item) => (
+        <div key={item.id}>
+          <div>{item.title}</div>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <button onClick={() => statusTodo(item.id)}>Open/Close</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
