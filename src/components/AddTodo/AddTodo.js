9ee2c9d046df47344d8ac041e328ea751b29a531
Vladimir RM <shvdrummer@gmail.com>
2022-12-19 09:23:53 +0200

    add button for AddTodo

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,14 +1,16 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const AddTodo = () => {
-
-  const [ value,setValue]= useState('')
+  const [value, setValue] = useState("");
   return (
     <div>
       <input type="text"
-       />
+       value={value}
+       placeholder='Enter something...'
+       onChange={(e)=>setValue(e.target.value)} />
+       <button></button>
     </div>
-  )
-}
+  );
+};
 
-export default AddTodo
+export default AddTodo;
