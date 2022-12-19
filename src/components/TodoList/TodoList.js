feb4875e8afe29f9ec9b84210f65384efd7f9a4e
Vladimir RM <shvdrummer@gmail.com>
2022-12-19 09:33:17 +0200

    add hooke useState for TodoLIst

---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,15 @@
+import React,{useState} from 'react'
+
+const TodoList = () => {
+
+const [edit,setEdit]=useState(null)
+const [value,setValue]= useState('')
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TodoList
