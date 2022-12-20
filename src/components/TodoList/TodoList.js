754ea136608067b0b1827b0d9e685452c2afb97d
Vladimir RM <shvdrummer@gmail.com>
2022-12-20 16:48:59 +0200

    add props todo settodo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react'
 
-const TodoList = () => {
+const TodoList = ({todo,setTodo}) => {
 const [edit,setEdit]=useState(null)
 const [value,setValue]=useState('')
 
 
 
 function deleteTodo(id){
-  
+     const newTodo = [...todo].filter((item)=>item.id !==id)
+     set
 }
 
 
