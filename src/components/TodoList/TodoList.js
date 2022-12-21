444c0518e91907d570ce3eaab1dfa2d71e2deb80
Vladimir RM <shvdrummer@gmail.com>
2022-12-21 08:57:17 +0200

    add button deleteTodo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,16 @@
 import React,{useState} from 'react'
 
 const TodoList = ({todo,setTodo}) => {
-
+      
 
   
   return (
-    <div>{todo}</div>
+    <div>{todo.map((item)=>(
+      <div key={item.id}>
+        <div>{item.title}</div>
+        <button onClick={()=>deleteTodo(item.id)}>Delete</button>
+      </div>
+    ))}</div>
   )
 }
 
