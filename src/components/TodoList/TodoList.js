397366bb2619e125028a388ea3835543cc199cbb
Vladimir RM <shvdrummer@gmail.com>
2022-12-22 09:08:56 +0200

    create func delete todo

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,30 +1,26 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const TodoList = ({todo,setTodo}) => {
+const TodoList = ({ todo, setTodo }) => {
+  const [edit, setEdit] = useState(null);
+  const [value, setValue] = useState("");
 
-  const [edit,setEdit]= useState(null)
-  const [value,setValue]= useState('')
-
-function deleteTodo(id){
-  
-}
-function statusTodo(id){
-
-}
-
+  function deleteTodo(id) {
+    const newTodo = [...todo].filter((item) => item.id !== id);
+    setTodo(newTodo);
+  }
+  function statusTodo(id) {}
 
   return (
     <div>
-      {todo.map((item)=>(
+      {todo.map((item) => (
         <div key={item.id}>
           <div>{item.title}</div>
-          <button onClick={()=>deleteTodo(item.id)}>Delete</button>
-          <button onClick={()=>statusTodo(item.id)}>Open/Close</button>
+          <button onClick={() => deleteTodo(item.id)}>Delete</button>
+          <button onClick={() => statusTodo(item.id)}>Open/Close</button>
         </div>
       ))}
-      
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
